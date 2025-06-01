770afe98fceb24cae916700255dade02108d2173
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-05-31 19:11:12 -0500

    dynamic join/leave buttons w/ state updates

--- src/components/event/EventCard.js
+++ src/components/event/EventCard.js
@@ -4,9 +4,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { Card, Button } from 'react-bootstrap';
 import { useRouter } from 'next/navigation';
+import { useAuth } from '@/utils/context/authContext';
+import { joinEvent, leaveEvent } from '../../utils/sample-data/eventData';
 
-export default function EventCard({ id, game, description, date, time, organizer, deleteThisEvent }) {
+export default function EventCard({ id, game, description, date, time, organizer, deleteThisEvent, joined, setEvents }) {
   const router = useRouter();
+  const { user } = useAuth();
+  const handleJoin = () => joinEvent(id, user.uid, setEvents);
+  const handleLeave = () => leaveEvent(id, user.uid, setEvents);
 
   return (
     <div>
@@ -17,6 +22,8 @@ export default function EventCard({ id, game, description, date, time, organizer
         <Card.Body>
           <Card.Title>By: {organizer.uid}</Card.Title>
           <Card.Text>{game.title}</Card.Text>
+
+          {/* edit btn */}
           <Button
             onClick={() => {
               router.push(`/events/edit/${id}`);
@@ -25,9 +32,22 @@ export default function EventCard({ id, game, description, date, time, organizer
           >
             Edit
           </Button>
+
+          {/* delete btn */}
           <Button onClick={() => deleteThisEvent(id)} variant="danger">
             Delete
           </Button>
+
+          {/* join/leave btn */}
+          {joined ? (
+            <Button onClick={handleLeave} variant="warning">
+              Leave
+            </Button>
+          ) : (
+            <Button onClick={handleJoin} variant="success">
+              Join
+            </Button>
+          )}
         </Card.Body>
         <Card.Footer className="text-muted">{description}</Card.Footer>
       </Card>
@@ -43,4 +63,6 @@ EventCard.propTypes = {
   time: PropTypes.string.isRequired,
   organizer: PropTypes.string.isRequired,
   deleteThisEvent: PropTypes.func.isRequired,
+  joined: PropTypes.bool.isRequired,
+  setEvents: PropTypes.func.isRequired,
 };
